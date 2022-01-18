178a557837aab163e6ef04a89cb458dfb76346e2
hojun <hoojun78@gmail.com>
2022-01-18 12:47:32 +0900

    room3

--- create_room/src/main/java/com/example/inhastudy/domain/Room.java
+++ create_room/src/main/java/com/example/inhastudy/domain/Room.java
@@ -3,6 +3,8 @@ package com.example.inhastudy.domain;
 import com.example.inhastudy.models.RoomRequestDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -10,89 +12,96 @@ import java.util.Date;
 @Getter
 @NoArgsConstructor
 @Entity
+@DynamicInsert
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false)
-    private String room_title;
+    private String roomTitle;
 
     @Column(nullable = false)
-    private java.sql.Date re_start_date;
+    private java.sql.Date reStartDate;
 
     @Column(nullable = false)
-    private java.sql.Date re_end_date;
+    private java.sql.Date reEndDate;
 
     @Column(nullable = false)
     private String category;
 
-    @Column(nullable = false)
-    private Integer max_join;
+    @ColumnDefault("1")
+    private Integer curJoin;
 
     @Column(nullable = false)
-    private java.sql.Date ac_start_date;
+    private Integer maxJoin;
 
     @Column(nullable = false)
-    private java.sql.Date ac_end_date;
+    private java.sql.Date acStartDate;
+
+    @Column(nullable = false)
+    private java.sql.Date acEndDate;
 
     @Column(nullable = false)
     private String location;
 
     @Column(nullable = false)
-    private String study_count;
+    private String studyCount;
 
     @Column(nullable = false)
-    private Integer study_time;
+    private Integer studyTime;
 
     @Column(nullable = false)
-    private String active_type;
+    private String activeType;
 
     @Column(nullable = true)
     private String info;
 
-    public Room(String room_title, java.sql.Date re_start_date, java.sql.Date re_end_date, String category, Integer max_join, java.sql.Date ac_start_date, java.sql.Date ac_end_date, String location, String study_count, Integer study_time, String active_type, String info) {
-        this.room_title=room_title;
-        this.re_start_date = re_start_date;
-        this.re_end_date=re_end_date;
+    public Room(String roomTitle, java.sql.Date reStartDate, java.sql.Date reEndDate, String category, Integer curJoin, Integer maxJoin, java.sql.Date acStartDate, java.sql.Date acEndDate, String location, String studyCount, Integer studyTime, String activeType, String info) {
+        this.roomTitle=roomTitle;
+        this.reStartDate = reStartDate;
+        this.reEndDate=reEndDate;
         this.category = category;
-        this.max_join=max_join;
-        this.ac_start_date=ac_start_date;
-        this.ac_end_date=ac_end_date;
+        this.curJoin=curJoin;
+        this.maxJoin=maxJoin;
+        this.acStartDate=acStartDate;
+        this.acEndDate=acEndDate;
         this.location=location;
-        this.study_count=study_count;
-        this.study_time=study_time;
-        this.active_type=active_type;
+        this.studyCount=studyCount;
+        this.studyTime=studyTime;
+        this.activeType=activeType;
         this.info=info;
     }
 
     public Room(RoomRequestDto requestDto) {
-        this.room_title = requestDto.getRoom_title();
-        this.re_start_date=requestDto.getRe_start_date();
-        this.re_end_date = requestDto.getRe_end_date();
+        this.roomTitle = requestDto.getRoomTitle();
+        this.reStartDate=requestDto.getReStartDate();
+        this.reEndDate = requestDto.getReEndDate();
         this.category = requestDto.getCategory();
-        this.max_join = requestDto.getMax_join();
-        this.ac_start_date = requestDto.getAc_start_date();
-        this.ac_end_date = requestDto.getAc_end_date();
+        this.curJoin = requestDto.getCurJoin();
+        this.maxJoin = requestDto.getMaxJoin();
+        this.acStartDate = requestDto.getAcStartDate();
+        this.acEndDate = requestDto.getAcEndDate();
         this.location = requestDto.getLocation();
-        this.study_count= requestDto.getStudy_count();
-        this.study_time = requestDto.getStudy_time();
-        this.active_type = requestDto.getActive_type();
+        this.studyCount= requestDto.getStudyCount();
+        this.studyTime = requestDto.getStudyTime();
+        this.activeType = requestDto.getActiveType();
         this.info = requestDto.getInfo();
     }
 
     public void update(RoomRequestDto roomRequestDto) {
-        this.room_title=roomRequestDto.getRoom_title();
-        this.re_start_date = roomRequestDto.getRe_start_date();
-        this.re_end_date=roomRequestDto.getRe_end_date();
+        this.roomTitle=roomRequestDto.getRoomTitle();
+        this.reStartDate = roomRequestDto.getReStartDate();
+        this.reEndDate=roomRequestDto.getReEndDate();
         this.category = roomRequestDto.getCategory();
-        this.max_join=roomRequestDto.getMax_join();
-        this.ac_start_date=roomRequestDto.getAc_start_date();
-        this.ac_end_date=roomRequestDto.getAc_end_date();
+        this.curJoin = roomRequestDto.getCurJoin();
+        this.maxJoin=roomRequestDto.getMaxJoin();
+        this.acStartDate=roomRequestDto.getAcStartDate();
+        this.acEndDate=roomRequestDto.getAcEndDate();
         this.location=roomRequestDto.getLocation();
-        this.study_count=roomRequestDto.getStudy_count();
-        this.study_time=roomRequestDto.getStudy_time();
-        this.active_type=roomRequestDto.getActive_type();
+        this.studyCount=roomRequestDto.getStudyCount();
+        this.studyTime=roomRequestDto.getStudyTime();
+        this.activeType=roomRequestDto.getActiveType();
         this.info=roomRequestDto.getInfo();
     }
 }
--- create_room/src/main/java/com/example/inhastudy/models/RoomRequestDto.java
+++ create_room/src/main/java/com/example/inhastudy/models/RoomRequestDto.java
@@ -8,32 +8,34 @@ import java.sql.Date;
 @Getter
 @NoArgsConstructor
 public class RoomRequestDto {
-    private String room_title;
-    private java.sql.Date re_start_date;
-    private java.sql.Date re_end_date;
+    private String roomTitle;
+    private java.sql.Date reStartDate;
+    private java.sql.Date reEndDate;
     private String category;
-    private Integer max_join;
-    private java.sql.Date ac_start_date;
-    private java.sql.Date ac_end_date;
+    private Integer curJoin;
+    private Integer maxJoin;
+    private java.sql.Date acStartDate;
+    private java.sql.Date acEndDate;
     private String location;
-    private String study_count;
-    private Integer study_time;
-    private String active_type;
+    private String studyCount;
+    private Integer studyTime;
+    private String activeType;
     private String info;
 
 
-    public RoomRequestDto(String room_title, java.sql.Date re_start_date, java.sql.Date re_end_date, String category, Integer max_join, java.sql.Date ac_start_date, java.sql.Date ac_end_date, String location, String study_count, Integer study_time, String active_type, String info) {
-        this.room_title=room_title;
-        this.re_start_date = re_start_date;
-        this.re_end_date=re_end_date;
+    public RoomRequestDto(String roomTitle, java.sql.Date reStartDate, java.sql.Date reEndDate, String category, Integer curJoin, Integer maxJoin, java.sql.Date acStartDate, java.sql.Date acEndDate, String location, String studyCount, Integer studyTime, String activeType, String info) {
+        this.roomTitle=roomTitle;
+        this.reStartDate = reStartDate;
+        this.reEndDate=reEndDate;
         this.category = category;
-        this.max_join=max_join;
-        this.ac_start_date=ac_start_date;
-        this.ac_end_date=ac_end_date;
+        this.curJoin = curJoin;
+        this.maxJoin=maxJoin;
+        this.acStartDate=acStartDate;
+        this.acEndDate=acEndDate;
         this.location=location;
-        this.study_count=study_count;
-        this.study_time=study_time;
-        this.active_type=active_type;
+        this.studyCount=studyCount;
+        this.studyTime=studyTime;
+        this.activeType=activeType;
         this.info=info;
     }
 }
